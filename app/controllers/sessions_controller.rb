9d46a3d1834efa7b29f81bd240e018fbd0a22b36
ElleeB <laura.trager@gmail.com>
2018-08-22 16:02:10 -0400

    Login success via sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,21 +5,26 @@ class SessionsController < ApplicationController
 
   def create
     # log in user with the correct password
-
     # if user has entered login info
+    # if params[:email] && params[:password]
       # check the login info (name + password) against database
-       # if match
+    if user = User.find_by(email: params[:user][:email]).try(:authenticate, params[:user][:password])
+      # if match
         # create session and redirect_to user_path
-      # else
-        # redirect_to /login
-        # flash[:notice]
-      # end
-    # elsif not entered login info
+      session[:user_id] = user.id
+
+      redirect_to user_path(user)
+    else
       # redirect_to /login
       # flash[:notice]
+      redirect_to '/login', notice: "You must enter a valid email and password"
+    end
+    # elsif not entered login info
+    # else
+    #   redirect_to '/login', notice: "You must enter a valid email and password"
     # end
-
-
-    redirect_to user_path
   end
+
+  private
+
 end
