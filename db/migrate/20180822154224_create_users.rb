c9f69ea6732c6830d96ca6c978194da64c55817a
ElleeB <laura.trager@gmail.com>
2018-08-22 13:58:51 -0400

    Create user migration

--- db/migrate/20180822154224_create_users.rb
+++ db/migrate/20180822154224_create_users.rb
@@ -1,6 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string :name
+      t.string :username
+      t.string :email
+      t.string :about
+      t.string :image
+      t.string :password_digest
 
       t.timestamps
     end
