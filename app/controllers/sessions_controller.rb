ddfa5d4083db2e77cba36d64a4026cc10d86adbc
ElleeB <laura.trager@gmail.com>
2018-08-22 15:36:57 -0400

    Add notes to sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,22 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # log in user with the correct password
+
+    # if user has entered login info
+      # check the login info (name + password) against database
+       # if match
+        # create session and redirect_to user_path
+      # else
+        # redirect_to /login
+        # flash[:notice]
+      # end
+    # elsif not entered login info
+      # redirect_to /login
+      # flash[:notice]
+    # end
+
+
     redirect_to user_path
   end
 end
