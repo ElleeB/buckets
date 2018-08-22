fee7ce16b3b6d1c10bd8b8f39b94695bf676a4f5
ElleeB <laura.trager@gmail.com>
2018-08-22 15:16:41 -0400

    Add create#user code but instance variable fails in view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,21 @@ class UsersController < ApplicationController
   end
 
   def create
-    redirect_to user_path
+    # raise params.inspect
+    if @user = User.create(user_params)
+
+      redirect_to user_path(@user)
+    else
+      redirect_to new_user_path
+    end
   end
 
   def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
   end
 end
