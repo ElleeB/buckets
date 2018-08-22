e0d108e4ed5a9eecd608e92c0db4797522fcb204
ElleeB <laura.trager@gmail.com>
2018-08-22 14:30:36 -0400

    Add welcome#home landing page/route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :bucket_items
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/', to: "welcome#home"
 end
