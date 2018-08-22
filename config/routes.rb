c70d9740ff99dd85ca4fd03906285bb15b3dd505
ElleeB <laura.trager@gmail.com>
2018-08-22 14:37:09 -0400

    Add route notes for user from home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :bucket_items
   resources :users
 
+  # :user login => sessions#new | signup => users#new | or omniauth route
+
   get '/', to: "welcome#home"
 end
