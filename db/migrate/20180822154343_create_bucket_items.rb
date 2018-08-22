61c808e88eabd5d7fb83a38f62f01d1be80336da
ElleeB <laura.trager@gmail.com>
2018-08-22 14:01:41 -0400

    Create bucket_items migration

--- db/migrate/20180822154343_create_bucket_items.rb
+++ db/migrate/20180822154343_create_bucket_items.rb
@@ -1,6 +1,10 @@
 class CreateBucketItems < ActiveRecord::Migration[5.2]
   def change
     create_table :bucket_items do |t|
+      t.string :title
+      t.string :description
+      t.string :due_date
+      t.string :location
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_22_154224) do
+ActiveRecord::Schema.define(version: 2018_08_22_154343) do
+
+  create_table "bucket_items", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "due_date"
+    t.string "location"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
