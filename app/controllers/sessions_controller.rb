84ee9e5f56b2e6385bc5bf8e692f6bc48098f2e7
ElleeB <laura.trager@gmail.com>
2018-08-22 21:43:01 -0400

    Remove notes from sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,27 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # log in user with the correct password
-    # if user has entered login info
-    # if params[:email] && params[:password]
-      # check the login info (name + password) against database
-
-      ##=> try using ar validations and callbacks here
+    ##=> try using ar validations and callbacks here
     if user = User.find_by(email: params[:user][:email]).try(:authenticate, params[:user][:password])
-      # if match
-        # create session and redirect_to user_path
       session[:user_id] = user.id
 
       redirect_to user_path(user)
     else
-      # redirect_to /login
-      # flash[:notice]
       redirect_to '/login', notice: "You must enter a valid email and password"
     end
-    # elsif not entered login info
-    # else
-    #   redirect_to '/login', notice: "You must enter a valid email and password"
-    # end
   end
 
   private
