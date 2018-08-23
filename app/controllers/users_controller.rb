59ef8c1114a4472911b270f074f75ee56e494eff
ElleeB <laura.trager@gmail.com>
2018-08-22 20:50:04 -0400

    Refactor users#create validation + flash[:notice]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def create
     # raise params.inspect
-    if user = User.create(user_params)
+    if user = User.create(user_params).valid?
       #=> even with unacceptable info, a user is still being created
       session[:user_id] = user.id
 
