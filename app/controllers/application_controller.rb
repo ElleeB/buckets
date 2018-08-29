c1db995ef2a9ba2585b6eeea984ec9ab053accee
ElleeB <laura.trager@gmail.com>
2018-08-29 14:00:05 -0400

    Refactor welcome#home with logged_in? instead of current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id])
   end
-  #
-  # def logged_in?
-  #    !!session[:user_id]
-  #  end
+
+  def logged_in?
+     !!session[:user_id]
+   end
 
   def current_bucket_item
     @bucket_item = BucketItem.find(params[:id])
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,9 @@
 class WelcomeController < ApplicationController
   def home
-    if current_user
+    if logged_in?
       redirect_to user_path(current_user)
+    else
+      render :home
     end
   end
 end
