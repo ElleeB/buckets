594c3283097217e426ee5eb23060ef910174c241
ElleeB <laura.trager@gmail.com>
2018-08-29 21:34:22 -0400

    Add countdown_test method to be refactored

--- app/models/bucket_item.rb
+++ app/models/bucket_item.rb
@@ -7,4 +7,24 @@ class BucketItem < ApplicationRecord
   def formated_date
     due_date.strftime("%B %d, %Y")
   end
+
+  # def refactor_for_calculation(date)
+  #   # turn due_date and Date.today = (year, month, day)
+  #   date.strftime("%Y, %m, %d")
+  # end
+  #
+  # def countdown
+  #   "#{refactor_for_calculation(due_date)}" - "#{refactor_for_calculation(Date.today)}"
+  # end
+  def countdown_test
+    today = Date.today.to_time
+
+    due = "Fri, 31 Aug 2018".to_time
+
+    day = 86400
+
+    puts (due - today)/day
+  end
+
+
 end
