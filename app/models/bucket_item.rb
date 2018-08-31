3966f497ffb981064d05230b11bd5c3ceba22f62
ElleeB <laura.trager@gmail.com>
2018-08-31 09:16:19 -0400

    Refactor clean up

--- app/models/bucket_item.rb
+++ app/models/bucket_item.rb
@@ -8,29 +8,11 @@ class BucketItem < ApplicationRecord
     due_date.strftime("%B %d, %Y")
   end
 
-  # def refactor_for_calculation(date)
-  #   # turn due_date and Date.today = (year, month, day)
-  #   date.strftime("%Y, %m, %d")
-  # end
-  #
-  # def countdown
-  #   "#{refactor_for_calculation(due_date)}" - "#{refactor_for_calculation(Date.today)}"
-  # end
   def countdown
-    today_date = Date.today.to_time
-
-    due_date = self.due_date.to_time
-
-    day = 86400
-
-    ((due_date - today_date)/day).to_i
+    day_in_seconds = 86400
+    dates_difference = (self.due_date.to_time) - (Date.today.to_time)
+    (dates_difference/day_in_seconds).to_i
   end
-
-  # def countdown
-  #   day_in_seconds = 86400
-  #   dates_difference = (Date.today.to_time) - ("Fri, 31 Aug 2018".to_time)
-  #   (dates_difference/day_in_seconds).to_i
-  # end
 
 
 end
