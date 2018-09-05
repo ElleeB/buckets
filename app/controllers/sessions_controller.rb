7403cacd70b06b6e6f15abdd9737549367fc9995
ElleeB <laura.trager@gmail.com>
2018-09-04 21:05:47 -0400

    Enable omniauth facebook authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,24 +4,34 @@ class SessionsController < ApplicationController
   end
 
   def create
-
     # if used email and password to log in
     if params[:email].present?
-      if user = User.find_by(email: params[:user][:email]).try(:authenticate, params[:user][:password])
+      if @user = User.find_by(email: params[:user][:email]).try(:authenticate, params[:user][:password])
         session[:user_id] = user.id
 
-        redirect_to user_path(user)
+        redirect_to user_path(@user)
       else
         ##=> try using ar validations and callbacks here
         redirect_to '/login', notice: "You must enter a valid email and password"
       end
     # if used Facebook to log in
     else
-      user = User.find_or_create_by(uid: auth['uid']) do |user|
-        user.name = auth['info']['name']
-      end
+      if @user = User.find_by(uid: auth['uid'])
 
-      session[:user_id] = user.id
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        @user = User.create(
+          uid: auth['uid'],
+          name: auth['info']['name'],
+          email: auth['info']['email'],
+          image: auth['info']['image'],
+        )
+        @user.save(validate: false)
+
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      end
     end
   end
 
@@ -30,7 +40,4 @@ class SessionsController < ApplicationController
 
     redirect_to '/'
   end
-
-  private
-
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_27_213746) do
+ActiveRecord::Schema.define(version: 2018_09_03_193432) do
 
   create_table "bucket_items", force: :cascade do |t|
     t.string "title"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2018_08_27_213746) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid"
   end
 
 end
