72a137c7cbf3a6e3e8475971f683aaaab4bcdf73
ElleeB <laura.trager@gmail.com>
2018-09-07 11:06:57 -0400

    Add basic action/view stubs

--- app/models/to_do_list.rb
+++ app/models/to_do_list.rb
@@ -1,4 +1,22 @@
 class ToDoList < ApplicationRecord
   belongs_to :bucket_item
   belongs_to :user
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
