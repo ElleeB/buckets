753c1fac66cf0b8fd05abd8f3091e545ac7f5044
ElleeB <laura.trager@gmail.com>
2018-09-09 14:19:47 -0400

    Create table activities and fix naming issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_03_193432) do
+ActiveRecord::Schema.define(version: 2018_09_09_181605) do
 
   create_table "activities", force: :cascade do |t|
     t.string "title"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 2018_09_03_193432) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
