c5022c489acb749af9be2966cf54932804f5422d
ElleeB <laura.trager@gmail.com>
2018-09-09 21:49:15 -0400

    Set up basic routes, actions, views items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,23 @@
 class ItemsController < ApplicationController
+  belongs_to :list
+  belongs_to :user
+  belongs_to :activity
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
