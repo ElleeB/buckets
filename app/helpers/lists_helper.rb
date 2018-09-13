4ebbfdd0ad3e16cf934ab43fbb0c74ed9a9874a7
ElleeB <laura.trager@gmail.com>
2018-09-13 15:01:16 -0400

    Add lists#incomplete_items?

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,2 +1,9 @@
 module ListsHelper
+
+  # def display_incomplete_items
+  #   @list.items.any? {|item| item.complete == false}
+  # end
+  def incomplete_items?
+    @list.items.any? {|item| item.complete == false}
+  end
 end
