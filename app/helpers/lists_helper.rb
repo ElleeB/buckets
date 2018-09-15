b11ca1e7d4971c13dca133a608e3c7cf7b672499
ElleeB <laura.trager@gmail.com>
2018-09-14 20:32:55 -0400

    Continue problem solving #display_incomplete_items

--- app/helpers/lists_helper.rb
+++ app/helpers/lists_helper.rb
@@ -1,21 +1,22 @@
 module ListsHelper
 
+  # def display_incomplete_items
+  #   @list.items.any? {|item| item.complete == false}
+  # end
+  # def incomplete_items?
+  #   @list.items.any? {|item| item.complete == false}
+  # end
+
+
   def display_incomplete_items(list)
-    if list.items.any? do |item|
-        incomplete = []
-        if item.complete == false
-          incomplete << item
-        form_for list do |f|
-          incomplete.each do |item|
-
-            @checkbox = "<h4><p> check_box_tag 'complete[item_ids][]', '#{item.id}' '#{item.name.titlecase}'</p></h4>".html_safe
-
-          end
-
-        end
-
-      end
+    display = []
+    incomplete = list.items.reject {|item| item.complete == true}
+    incomplete.collect do |item|
+      display << raw("<h4><p> check_box_tag 'complete[item_ids][]' '#{item.id}' '#{item.name.titlecase}' </p></h4>")
     end
+    display
+    # => [#<Item id: 5, name: "item 1", list_id: 2, user_id: 2, activity_id: 2, complete: false>,
+    #<Item id: 6, name: "item 2", list_id: 2, user_id: 2, activity_id: 2, complete: false>,
+    #<Item id: 7, name: "item 3", list_id: 2, user_id: 2, activity_id: 2, complete: false>]
   end
-end
 end
