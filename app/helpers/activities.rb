b921e36764339d2a69351f4100f21c9f400e47d5
ElleeB <laura.trager@gmail.com>
2018-09-25 20:52:08 -0400

    Add notes

--- app/helpers/activities.rb
+++ app/helpers/activities.rb
@@ -1,6 +1,2 @@
 module ActivitiesHelper
-
-  # def incomplete_activities?
-  #   @activity.lists.any? {|list| list.complete == false}
-  # end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,2 @@
 module UsersHelper
-
-  def display_incomplete_activities(activities)
-    incompletes = activities.reject { |activity| activity.complete == true }
-    incompletes.each do |activity|
-      display << "<h4><li> #{link_to} #{activity.title}, activity_path(activity.id) </h4>"
-    end
-    display
-  end
 end
