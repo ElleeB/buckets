96fce49b6e4b28a9f3edbbca4b9194f55db558b7
ElleeB <laura.trager@gmail.com>
2018-10-03 09:39:08 -0400

    Refactor and remove logic users/show utilize helper methods + remove partials

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -7,4 +7,18 @@ module ActivitiesHelper
       tag.p "#{activity.countdown} days until this drop is due!"
     end
   end
+
+  def accomplished_activities(activities)
+    accomplished = activities.reject { |activity| !activity.complete }
+    !accomplished.empty?
+  end
+
+  def pending_activities(activities)
+    pending = activities.reject { |activity| activity.complete }
+    !pending.empty?
+  end
+
+  def any_activities?(activities)
+    activities.empty?
+  end
 end
