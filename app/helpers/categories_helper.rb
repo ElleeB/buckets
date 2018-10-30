9fd5f42057f3936c61bd116812bc4ef51a549507
ElleeB <laura.trager@gmail.com>
2018-10-30 10:48:49 -0400

    Revert "Complete requirements for project"
    This reverts commit 3509851a354008b60a547ffc7274f41f9fec87e2.

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,14 +1,2 @@
 module CategoriesHelper
-
-  def current_user_categories
-    @current_user.categories
-  end
-
-  def user_category_activities(category)
-    current_user_categories.find do |user_category|
-      user_category.id == category.id
-    end
-  end
-
-
 end
