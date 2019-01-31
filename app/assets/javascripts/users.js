55d5ebc2c137e99e0734f2930294a0eae9153cc0
ElleeB <laura.trager@gmail.com>
2019-01-31 14:36:44 -0500

    Refactor view all drops button to change text to 'hide' once clicked and to clear drops and reset text once reclicked

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,13 +1,25 @@
 $(function() {
-  $("#all-drops").on('click', function(event) {
-    let userID = this.dataset.userId
-    $.get(`/users/${userID}/activities`, function(data) {
-      let activities = data
-      let div = $("#show-drop-button")
-      activities.forEach(function(activity) {
-        div.append(`<ul><li>${activity.title}</li></ul>`)
-        // need to ensure that it doesn't repeat append
-      })
-    })
+  $("#all-drops-button").on('click', function() {
+    let div = $("#list-drops")
+    // if button says view all drops
+    if ($("#all-drops-button")[0].innerText === "View All Drops") {
+      // if no drops visible
+      if ($("#list-drops").empty()) {
+        let userID = this.dataset.userId
+        $.get(`/users/${userID}/activities`, function(data) {
+          let activities = data
+          activities.forEach(function(activity) {
+            div.append(`<ul><li>${activity.title}</li></ul>`)
+            // need to ensure that it doesn't repeat append
+            //  + turn innerText into 'hide my drops'
+            $("#all-drops-button")[0].innerText = "Hide My Drops"
+          })
+        })
+      }
+    // if button says hide my drops
+    } else {
+      $("#list-drops")[0].innerHTML = ""
+      $("#all-drops-button")[0].innerText = "View All Drops"
+    }
   })
 })
